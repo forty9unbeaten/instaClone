2a5af4f7e809698dd201de9b17f8d588e1d36acc
Rob Spears <rob.sp3ars@gmail.com>
2020-07-08 23:59:00 -0400

    Add homepage view that renders homepage

--- instaPost/views.py
+++ instaPost/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Post
 
 # Create your views here.
+
+
+class HomepageView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(
+            request,
+            'index.html',
+            {
+                'posts': posts
+            })
