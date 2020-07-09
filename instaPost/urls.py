0b7b18e31efd0a54ab904d23c561e0257fe574e3
Chelsea White <chewh115@gmail.com>
2020-07-09 14:30:11 -0400

    Uploading new post is working

--- instaPost/urls.py
+++ instaPost/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 url_patterns = [
-    path('', views.HomepageView.as_view(), name='home')
+    path('', views.HomepageView.as_view(), name='home'),
+    path('newpost/', views.newpost, name='newPost')
 ]
