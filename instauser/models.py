239cbad316427fccc19d82ac93b556947c1a1604
Chelsea White <chewh115@gmail.com>
2020-07-10 11:10:32 -0400

    Updates user model to include: bio, email + url

--- instauser/models.py
+++ instauser/models.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class InstaUser(AbstractUser):
-    display_name = models.CharField(max_length=50, null=True, blank=True)
+    display_name = models.CharField(max_length=50, null=True)
     following = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='follow')
+    bio = models.TextField(blank=True)
+    email = models.EmailField(max_length=150)
+    url = models.URLField(blank=True)
 
-    REQUIRED_FIELDS = ['display_name']
+    REQUIRED_FIELDS = ['display_name', 'email']
