a92009bd303be6f1eac7044795b9f2fdf17c412c
Rob Spears <rob.sp3ars@gmail.com>
2020-07-12 16:35:11 -0400

    Add views to like and unlike posts

--- instaPost/views.py
+++ instaPost/views.py
@@ -17,6 +17,7 @@ class HomepageView(View):
                 'posts': posts
             })
 
+
 @login_required(login_url='/login/')
 def newpost(request):
     if request.method == 'POST':
@@ -26,7 +27,22 @@ def newpost(request):
             new_post.user = request.user
             new_post.save()
             return HttpResponseRedirect(reverse('home'))
-    
+
     form = NewPostForm()
     return render(request, 'postUploadForm.html', {'form': form})
 
+
+@login_required(login_url='/login/')
+def like_post(request, post_id):
+    if request.method == 'POST':
+        post = Post.objects.get(id=post_id)
+        post.likes.add(request.user)
+        return HttpResponseRedirect(reverse('home'))
+
+
+@login_required(login_url='/login/')
+def unlike_post(request, post_id):
+    if request.method == 'POST':
+        post = Post.objects.get(id=post_id)
+        post.likes.remove(request.user)
+        return HttpResponseRedirect(reverse('home'))
