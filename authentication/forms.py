63a92254d21077c28b2cb7910aab5d45448cb89a
avelyno <kou2295@calu.edu>
2020-07-12 17:22:53 -0400

    Profile Edit Button functional

--- authentication/forms.py
+++ authentication/forms.py
@@ -9,3 +9,6 @@ class SignupForm(forms.Form):
     username = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput)
     display_name = forms.CharField(max_length=50)
+    bio = forms.CharField(max_length=140, widget=forms.Textarea, required=False)
+    email = forms.EmailField(max_length=150, required=False)
+    
--- instaPost/views.py
+++ instaPost/views.py
@@ -30,3 +30,36 @@ def newpost(request):
     form = NewPostForm()
     return render(request, 'postUploadForm.html', {'form': form})
 
+def like(request, post_id):
+    current_user = request.user
+    image=Post.objects.get(id=post_id)
+    new_like,created= Likes.objects.get_or_create(liker=current_user, image=image)
+    new_like.save()
+
+    return redirect('home')
+
+
+def new_like(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NewLikeForm(request.POST)
+       
+        post = request.POST['post']
+        user = request.POST['user']
+        like = Like.objects.filter(post=post, user=user)
+
+        #import pdb; pdb.set_trace()
+        # check whether it's valid:
+        if like:
+            like.delete()
+            return redirect(request.META.get('HTTP_REFERER'))
+        elif form.is_valid():
+            form.save()
+            # redirect to a new URL:
+            return redirect(request.META.get('HTTP_REFERER'))
+
+    else:
+        # if a GET (or any other method) we'll create a blank form
+        form = NewLikeForm()
+    
