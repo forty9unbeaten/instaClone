a9a84fd7999be0d8befa44c00852ee9637fc914d
Rob Spears <rob.sp3ars@gmail.com>
2020-07-14 22:29:11 -0400

    Fix followers bug

--- instauser/views.py
+++ instauser/views.py
@@ -6,37 +6,39 @@ from authentication.forms import SignupForm
 
 # Create your views here.
 
+
 def profilePage(request, id):
     posts = Post.objects.filter(user=id).filter(archived=False)
     countposts = posts.count()
     user = InstaUser.objects.get(id=id)
     followers = user.following.all()
-    countfollowers = followers.count() -1
+    countfollowers = followers.count() - 1
     if request.user.is_authenticated:
         myfollowers = request.user.following.all()
-        if user in myfollowers:
+        if request.user in followers:
             is_following = True
         else:
             is_following = False
         return render(
-                request, 
-                'profilePage.html', {
-                'posts': posts, 
-                'countposts': countposts,
-                'user': user, 
-                'countfollowers': countfollowers,
-                'myfollowers': myfollowers,
-                'is_following': is_following,
-                })    
-    return render(
-                request, 
-                'profilePage.html', {
-                'posts': posts, 
+            request,
+            'profilePage.html', {
+                'posts': posts,
                 'countposts': countposts,
                 'user': user,
                 'countfollowers': countfollowers,
-                })
-    
+                'myfollowers': myfollowers,
+                'is_following': is_following,
+            })
+    return render(
+        request,
+        'profilePage.html', {
+            'posts': posts,
+            'countposts': countposts,
+            'user': user,
+            'countfollowers': countfollowers,
+        })
+
+
 def EditProfile(request, id):
     user = InstaUser.objects.get(id=id)
     if request.method == 'POST':
@@ -50,23 +52,23 @@ def EditProfile(request, id):
         return HttpResponseRedirect(reverse('profilePage', args=(id,)))
     form = SignupForm(initial={
         'display_name': user.display_name,
-        'bio' : user.bio,
-        'email' : user.email,
+        'bio': user.bio,
+        'email': user.email,
     })
-    return render(request, 'generic_form.html', {'form': form})    
-    
-    
-    
+    return render(request, 'generic_form.html', {'form': form})
+
+
 def follow(request, id):
     user = request.user
     follow = InstaUser.objects.get(id=id)
-    user.following.add(follow)
-    user.save()
-    return HttpResponseRedirect(reverse('profilePage', args={id,}))
+    follow.following.add(user)
+    follow.save()
+    return HttpResponseRedirect(reverse('profilePage', args={id, }))
+
 
 def unfollow(request, id):
     user = request.user
     follow = InstaUser.objects.get(id=id)
-    user.following.remove(follow)
-    user.save()
-    return HttpResponseRedirect(reverse('profilePage', args={id,}))
+    follow.following.remove(user)
+    follow.save()
+    return HttpResponseRedirect(reverse('profilePage', args={id, }))
