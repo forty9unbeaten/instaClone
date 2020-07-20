7aedd2c3ee66514e20e84f9908ee3e3a2618b0f9
Chelsea White <chewh115@gmail.com>
2020-07-20 10:54:16 -0400

    Cleans up instaUser app, fixes edit profile bug, adds URL to profile/edit pages

--- instauser/views.py
+++ instauser/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from instauser.models import InstaUser
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from instaPost.models import Post
 from authentication.forms import SignupForm
+from instauser.helpers import get_user, get_posts_by_user
 
 # Create your views here.
 
 
 def profilePage(request, id):
     try:
-        user = InstaUser.objects.get(id=id)
+        user = get_user(id)
     except:
         # user can't be found with ID provided in URL
         return render(
@@ -19,7 +21,7 @@ def profilePage(request, id):
                 'code': '404'
             })
 
-    posts = Post.objects.filter(user=id).filter(archived=False)
+    posts = get_posts_by_user(id).filter(archived=False)
     countposts = posts.count()
     followers = user.following.all()
     countfollowers = followers.count() - 1
@@ -49,10 +51,11 @@ def profilePage(request, id):
         })
 
 
+@login_required
 def EditProfile(request, id):
 
     try:
-        user = InstaUser.objects.get(id=id)
+        user = get_user(id)
     except:
         return render(
             request,
@@ -61,27 +64,31 @@ def EditProfile(request, id):
                 'code': '404'
             }
         )
+    if user == request.user:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user.display_name = data['display_name']
+                user.bio = data['bio']
+                user.email = data['email']
+                user.url = data['url']
+                user.save()
+            return HttpResponseRedirect(reverse('profilePage', args=(id,)))
+        form = SignupForm(initial={
+            'username': user.username,
+            'display_name': user.display_name,
+            'bio': user.bio,
+            'email': user.email,
+            'url': user.url
+        })
+        return render(request, 'generic_form.html', {'form': form})
+    return HttpResponseRedirect(reverse('profilePage', args=(id,)))
 
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            user.display_name = data['display_name']
-            user.bio = data['bio']
-            user.email = data['email']
-            user.save()
-        return HttpResponseRedirect(reverse('profilePage', args=(id,)))
-    form = SignupForm(initial={
-        'display_name': user.display_name,
-        'bio': user.bio,
-        'email': user.email,
-    })
-    return render(request, 'generic_form.html', {'form': form})
-
-
+@login_required
 def follow(request, id):
     try:
-        follow = InstaUser.objects.get(id=id)
+        follow = get_user(id)
     except:
         return render(
             request,
@@ -95,10 +102,10 @@ def follow(request, id):
     follow.save()
     return HttpResponseRedirect(reverse('profilePage', args={id, }))
 
-
+@login_required
 def unfollow(request, id):
     try:
-        follow = InstaUser.objects.get(id=id)
+        follow = get_user(id)
     except:
         return render(
             request,
